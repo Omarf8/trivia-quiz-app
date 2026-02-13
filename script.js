89a4f3d5a5baea2341b0e008c9cb17850d0c3e10
Omarf8 <omarfigueroa223@gmail.com>
2026-02-13 11:08:57 -0600

    Fetch questions using Trivia API

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Simple way to display the next screen to play the game
 let startScreen = document.getElementById("start-screen")
 let quizScreen = document.getElementById("quiz")
 let startButton = document.getElementById("start-button")
@@ -7,3 +8,14 @@ startButton.addEventListener("click", function() {
     quizScreen.style.display = "block"
 })
 
+// Get the requested trivia info
+let questions = []
+
+async function fetchQuestions() {
+    const response = await fetch("https://opentdb.com/api.php?amount=10")
+    const data = await response.json()
+    questions = data
+    // console.log(questions)
+}
+
+// fetchQuestions()
