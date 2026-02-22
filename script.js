21336f6ab7cf041812d33f758a2db1ed9038792c
Omarf8 <omarfigueroa223@gmail.com>
2026-02-22 17:17:50 -0600

    Organize JS file

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-// Simple way to display the next screen to play the game
+// Global Variables
+let questions = [] // Store the requested trivia info
+let questionNum = 0 // Keep track of our current question
+let score = 0
+
+// DOM Selection
 let startScreen = document.getElementById("start-screen")
 let quizScreen = document.getElementById("quiz")
 let startButton = document.getElementById("start-button")
 
-startButton.addEventListener("click", function() {
-    startScreen.style.display = "none"
-    quizScreen.style.display = "block"
-})
+// Utility Functions
+function shuffleArray(arr) {
+    let i = arr.length - 1
+    while(i > 0) {
+        let randomIndex = Math.floor((Math.random() * (i + 1))) // Get index from 0-i
+        let tmp = arr[randomIndex]
+        arr[randomIndex] = arr[i]
+        arr[i] = tmp
+        i--;
+    }
+}
 
-// Get the requested trivia info
-let questions = []
-
+// API Functions
 async function fetchQuestions() {
     const response = await fetch("https://opentdb.com/api.php?amount=10")
     const data = await response.json()
@@ -18,9 +28,7 @@ async function fetchQuestions() {
     // console.log(questions)
 }
 
-// fetchQuestions()
-let questionNum = 0 // Keep track of our current question
-
+// Game Logic
 function populateAnswers() {
     let answerBoxes = document.querySelectorAll(".answer")
     let incorrectAnswer = questions[questionNum].incorrect_answers
@@ -35,13 +43,11 @@ function populateAnswers() {
     questionNum++; // Increment for the next time this func is called
 }
 
-function shuffleArray(arr) {
-    let i = arr.length - 1
-    while(i > 0) {
-        let randomIndex = Math.floor((Math.random() * (i + 1))) // Get index from 0-i
-        let tmp = arr[randomIndex]
-        arr[randomIndex] = arr[i]
-        arr[i] = tmp
-        i--;
-    }
-}
+// Event Listeners
+startButton.addEventListener("click", function() {
+    startScreen.style.display = "none"
+    quizScreen.style.display = "block"
+})
+
+// Initialize
+// fetchQuestions()
